5af456c550123896c0edb3558a936bc932f7aec4
MkGodh <fenryrvla@yandex.ru>
2024-04-08 20:39:22 +0300

    Добавьте файлы проекта.

---
+++ ChessDeckCoords/ChessDeckCoords.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <string>
+#include <cstring>
+#include <cmath>
+#include <iomanip>
+#include <math.h>
+#include <vector>
+
+using namespace std;
+int main() {
+
+    setlocale(LC_ALL, "RUS");
+
+    cout << "Введите координаты шахматной доски: \n";
+    char x, y;
+    cin >> x >> y;
+    if (((x >= 'A' && x <= 'H') || (x >= 'a' && x <= 'h')) && y >= '1' && y <= '8') {
+        cout << "Да, координаты попадают под шахматное поле!\n";
+    }
+    else {
+        cout << "Координаты не верны!\n";
+    }
+
+
+
+}
